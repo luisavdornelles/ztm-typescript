650b3828a2a81e39dfbf69f8afd27b192210c8fb
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-14 11:39:18 -0400

    Update variables.ts
    Variables & Basic Types

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -9,3 +9,40 @@
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 //
+
+const courseName: string = "TypeScript";
+const courseName2: string = 'TypeScript';
+const courseName3: string = `TypeScript`;
+
+const amount: number = 100;
+const fraction = 10.4;
+const oneThousant = 1e3;
+
+const allPermissions = 0o777;
+
+const hexByte = 0x0f;
+
+const binary = 0b00001;
+
+const bigInt = 9000n;
+
+const yes = true;
+const no = false;
+
+// completely missing
+const missing = undefined;
+
+// we know it's missing
+const empty = null;
+
+let someNumber = 10;
+someNumber = 20;
+// let someNumber = 50; // Error: Cannot redeclare block-scoped variable 'someNumber'
+
+// variable shadowing
+{ // block scope
+    let someNumber = 50;
+}
+
+let hello;
+// const whoops; // Error: Missing initializer in const declaration
