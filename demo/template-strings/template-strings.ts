2d6004e6484a979633ccd8319f733ca8917f1939
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-14 13:26:36 -0400

    Update template-strings.ts
    Template Strings

--- demo/template-strings/template-strings.ts
+++ demo/template-strings/template-strings.ts
@@ -7,3 +7,17 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 //
+
+function greet(msg) {
+    console.log(`hello ${msg}`);
+}
+
+greet("TypeScript");
+
+// We can also use expressions in the template literal.
+const kids = 2;
+const adults = 4;
+// Try to keep the expression short so it's easy to read the string.
+const totalPeople = `There are ${kids + adults} people`;
+
+console.log(`${totalPeople} to seat at the restaurant.`);
