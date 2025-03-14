31f4230753fd78f25acf9e0432534080b8a2b7f6
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-14 11:55:38 -0400

    Update compound-bool.ts
    Compound Boolean Expressions

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,4 +1,29 @@
 /* eslint-disable */
+
+// Run  pnpm ts-node demo/compound-bool/compound-bool.ts
+
 import { strict as assert } from "assert";
 
+// NOT
+const writing = true;
+const reading = !writing;
+assert.equal(reading, false);
 
+// OR
+const rating = 9;
+const favoriteMovie = false;
+
+const suggestMovie = rating > 8 || favoriteMovie;
+assert.equal(suggestMovie, true);
+
+// AND
+const age = 18;
+const isTeenager = age >= 12 && age < 20;
+assert.equal(isTeenager, true);
+
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
+assert.equal(extraFee, true);
