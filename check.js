45db3753e1166a582f1233ea87d5b9309ba7c3a2
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-16 14:52:28 -0400

    Update check.js

--- check.js
+++ check.js
@@ -3,8 +3,8 @@ const execSync = require('child_process').execSync;
 const path = process.argv[2];
 
 try {
-  execSync(`pnpm typecheck ${path}`, { stdio: [0, 1, 2] });
-  execSync(`pnpm eslint ${path}`, { stdio: [0, 1, 2] });
+  execSync(`pnpm run typecheck ${path}`, { stdio: [0, 1, 2] });
+  execSync(`pnpm run lint ${path}`, { stdio: [0, 1, 2] });
 } catch (_) {
 
 }
