64cb98368dca5ad26dfe50ba96e6b073d93ef99b
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-18 14:57:11 -0400

    Create location.ts

---
+++ app/weather/location.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_KEY = "67d9c11706027690288422sae27514a";
+
+export interface LocationInfo {
+    lat: string;
+    lon: string;
+    display_name: string;
+}
+
+export async function fetchLocationData(
+    apiUrl:string,
+    locationName: string
+): Promise<LocationInfo> {
+    const options = {
+        method: "GET",
+        url: apiUrl,
+        params: {
+            q: locationName,
+            api_key: API_KEY,
+        }
+    }
+
+    const response = await axios.request<LocationInfo[]>(options);
+
+    if (response.status !== 200) {
+        throw new Error(`Failed to fecth location data. API responded with status ${response.status}`);
+    }
+
+    if (response.data.length === 0) {
+        throw new Error(`Unable to find location information for ${locationName}`);
+    }
+
+    return response.data[0];
+}
