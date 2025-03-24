04942eb14bf2f98f74f51ef11cdc4e05cfb8f98a
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-24 10:08:32 -0400

    TS definitions for functions

---
+++ demo/dts/mylib.d.ts
@@ -0,0 +1,8 @@
+export function add(a: number, b: number, ...numbers: number[]): number;
+export function max(arr: number[]): number | null;
+export function quote(message: string): () => string;
+
+// const CaseKinds = ["lowercase", "uppercase"] as const;
+// export type CaseKind = (typeof CaseKinds)[number];
+export type CaseKind = "lowercase" | "uppercase";
+export function setCase(message: string, kind: CaseKind): string;
--- demo/dts/mylib.js
+++ demo/dts/mylib.js
@@ -1,3 +1,10 @@
+/**
+ * Calculates the total by adding two numbers.
+ * 
+ * @param {number} a - The first number to add.
+ * @param {number} b - The second number to add.
+ * @returns {number} The sum of the two numbers.
+ */
 function add(a, b, ...args) {
     let total = a + b;
     for (let n of args) {
@@ -6,6 +13,12 @@ function add(a, b, ...args) {
     return total;
 }
 
+/**
+ * Finds the maximum value in an array of numbers.
+ *
+ * @param {number[]} arr - The array of numbers to evaluate.
+ * @returns {number|null} The maximum value in the array, or null if the array is empty.
+ */
 function max(arr) {
     if (arr.length === 0) {
         return null;
@@ -13,6 +26,14 @@ function max(arr) {
     return arr.reduce((a, b) => Math.max(a, b));
 }
 
+/**
+ * Converts the case of a given string based on the specified kind.
+ *
+ * @param {string} message - The string to be converted.
+ * @param {"uppercase" | "lowercase"} kind - The case conversion type. Must be either "uppercase" or "lowercase".
+ * @returns {string} The converted string in the specified case.
+ * @throws {Error} Throws an error if the kind is not "uppercase" or "lowercase".
+ */
 function setCase(message, kind) {
     if (kind === "uppercase") {
         return message.toUpperCase();
@@ -23,6 +44,12 @@ function setCase(message, kind) {
     }
 }
 
+/**
+ * Wraps a given message in double quotes and returns a function that provides the quoted message.
+ *
+ * @param {string} message - The message to be quoted.
+ * @returns {function(): string} A function that returns the quoted message when called.
+ */
 function quote(message) {
     return () => { return `"${message}"` }
 }
