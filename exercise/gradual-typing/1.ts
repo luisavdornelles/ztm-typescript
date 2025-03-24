a70e4d07631273577638c635b0ffc997b210f052
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-24 11:00:19 -0400

    Gradual typing exercises

--- exercise/gradual-typing/1.ts
+++ exercise/gradual-typing/1.ts
@@ -8,19 +8,46 @@
 
 import { strict as assert } from "assert";
 
-function add(a, b) {
-  return a + b;
+// JavaScript
+{
+    console.log("JavaScript");
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    function add(a, b) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/restrict-plus-operands
+        return a + b;
+    }
+
+    console.log(add(5, 3));
+    assert.equal(add(1, 2), 3);
+
+    // These don't crash, but they do not produce expected results.
+    console.log(add("5", "3"));
+    console.log(add(null, NaN));
+    console.log(add(0, "1"));
+    console.log(add("1", 0));
+    console.log(add({}, 5));
+    console.log(add(2, [3]));
+    console.log(add(2, () => 3));
 }
 
-console.log(add(5, 3));
-assert.equal(add(1, 2), 3);
+// TypeScript
+{
+    console.log("\nTypeScript");
 
-// These don't crash, but they do not produce expected results.
-console.log(add("5", "3"));
-console.log(add(null, NaN));
-console.log(add(0, "1"));
-console.log(add("1", 0));
-console.log(add({}, 5));
-console.log(add(2, [3]));
-console.log(add(2, () => 3));
+    // types added to function signature
+    function add(a: number, b: number): number {
+        return a + b;
+    }
 
+    console.log(add(5, 3));
+    assert.equal(add(1, 2), 3);
+
+    // These are all now compiler errors. (Uncomment to see errors)
+    // console.log(add("5", "3"));
+    // console.log(add(null, NaN));
+    // console.log(add(0, "1"));
+    // console.log(add("1", 0));
+    // console.log(add({}, 5));
+    // console.log(add(2, [3]));
+    // console.log(add(2, () => 3));
+}
