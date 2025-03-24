9207a700cf989a20f3106d4eee4fabc79690c12b
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-24 16:27:58 -0400

    Db connection

--- app/submit-form/src/backend/db.ts
+++ app/submit-form/src/backend/db.ts
@@ -18,8 +18,29 @@ export interface UserRepository {
 // There could be another class to implement to Postgress for example
 export class SqliteUserRepository implements UserRepository {
     constructor(private readonly db: AsyncDatabase) {}
-    
+
     create(user: User): Promise<User> {}
     findByEmail(email: string): Promise<User | undefined> {}
     get(userId: number): Promise<User | undefined> {}
 }
+
+export async function connect(connectionString: string): Promise<AsyncDatabase> {
+    return await AsyncDatabase.open(connectionString);
+}
+
+export async function newDb(db: AsyncDatabase): Promise<void> {
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            email TEXT UNIQUE NOT NULL,
+            hashedPassword TEXT NOT NULL,
+            agreedToTerms BOOLEAN NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS sessions (
+            session_id UUID PRIMARY KEY,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+        );
+    `);
+}
+  
