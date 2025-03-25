066b5ca670c8ebb1f987a32006c54824241a0943
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-25 17:28:02 -0400

    Applying FE field validation to signin and signup

--- app/submit-form/src/frontend/signup.ts
+++ app/submit-form/src/frontend/signup.ts
@@ -6,19 +6,29 @@ const passwordField = document.getElementById("password") as HTMLInputElement;
 const passwordInvalidLabel = document.getElementById("invalid-password") as HTMLElement;
 
 const emailField = document.getElementById("email") as HTMLInputElement;
-const emailInvalidLabel = document.getElementById("invalid-email") as HTMLElement;
+const emailInvalidLabel = document.getElementById("invalid-email") as HTMLElement
+
+const agreeTosCheckBox = document.getElementById("agree-tos") as HTMLInputElement;
 
 const submitBtn = document.getElementById("form-submit");
 
 const errors = new FieldError();
 
 function updateSubmitBtn(): void {
-    if (errors.isEmpty() && emailField.value.length > 0 && passwordField.value.length > 0) {
+    if (errors.isEmpty() &&
+        emailField.value.length > 0 &&
+        passwordField.value.length > 0 &&
+        agreeTosCheckBox.checked
+    ) {
         submitBtn?.classList.remove("btn-disabled");
     } else {
         submitBtn?.classList.add("btn-disabled");
     }
 }
+
+agreeTosCheckBox.addEventListener("change", (_) => {
+    updateSubmitBtn();
+});
 
 emailField.addEventListener("input", (_) => {
     const usernameFailures = checkUsername(emailField.value);
@@ -44,3 +54,5 @@ passwordField.addEventListener("input", (_) => {
     updateSubmitBtn();
 });
 
+// set initial state
+agreeTosCheckBox.checked = false;
